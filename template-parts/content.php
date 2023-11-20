664bc372649fdd559f18111128b0b410ec44fcbc
Basila Medjedovic <basilamedjedovic@gmail.com>
2023-11-20 19:02:07 +0100

    custom fields

--- template-parts/content.php
+++ template-parts/content.php
@@ -18,6 +18,31 @@
 		width: 100%;
 		height: 400px;
 	}
+	.ingredient {
+        font-weight: bold;
+        color: #333;
+    }
+
+    .preparation {
+        margin-top: 10px;
+    }
+	.priprema{
+		font-weight: bold;
+		background: linear-gradient(to right, #990000, #ff6600);
+		padding: 10px;
+		color: #fff;
+		text-align: center;
+	}
+
+    .baking-time, .baking-temperature{
+        margin-top: 10px;
+        font-weight: bold;
+        color: #990000; /* Dark red color */
+    }
+	.basic{
+		display: flex;
+		justify-content: space-around;
+	}
 </style>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
@@ -62,7 +87,37 @@
 				wp_kses_post( get_the_title() )
 			)
 		);
-
+		?>
+		<div class="basic">
+			<div class="baking-time">
+				<h2>Vreme pecenja: <?php echo get_field('vreme_pecenja'); ?> min </h2>
+			</div>
+			<div class="baking-temperature">
+				<h2>Temperatura pecenja: <?php echo get_field('temperatura_pecenja'); ?> c</h2>
+			</div>
+		</div>
+		
+		<div class="ingredient">
+			<h2 class='priprema'>Sastojci</h2>
+			<?php
+				$ingredients = get_field('sastojak');
+				if ($ingredients) {
+					$ingredient_list = explode(',', $ingredients);
+					echo '<ul>';
+					foreach ($ingredient_list as $ingredient) {
+						echo '<li>' . esc_html(trim($ingredient)) . '</li>';
+					}
+					echo '</ul>';
+				}		
+			?>
+		</div>
+		
+		<div class="preparation">
+			<h2 class='priprema'>Priprema</h2>
+			<?php echo get_field('priprema'); ?>
+		</div>
+		
+		<?php
 		wp_link_pages(
 			array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'multimedijalnisistemi' ),
