9b353c84da96abda213bb195f6fec8353137fb5f
Basila Medjedovic <basilamedjedovic@gmail.com>
2023-12-02 15:30:36 +0100

    shownig custom posts on the main page

--- index.php
+++ index.php
@@ -62,6 +62,19 @@ get_header();
     max-width: 100%; /* Set the maximum width to 100% of the container */
     height: auto; /* Allow the height to adjust proportionally */
 }
+.naslovi{
+	color:#990000;
+    margin:auto 10px;
+    text-align: center;
+}
+#naslov{
+    justify-content: center; /* Center horizontally */
+    align-items: center; 
+}
+.div-naslov{
+    width:100%
+}
+
 
     /* Rest of your styles */
 </style>
@@ -119,7 +132,7 @@ get_header();
 	<h1>Inspiriši se i pripremi nešto dobro!</h1>
 </div>
 <main id="primary" class="site-main">
-
+    
     <?php
     if (have_posts()) :
 
@@ -131,9 +144,39 @@ get_header();
 			
             <?php
         endif;
-        
+        $query_args_1 = array(
+			'post_type' => 'wpll_recipe'
+		);
+		$query_1 = new WP_Query($query_args_1);
+		$merged_query = new WP_Query();
+		$merged_query->posts = array_merge($query_1->posts);
         /* Start the Loop */
         echo '<div class="article-container">';
+        echo"<div class='div-naslov'><h1 class='naslovi'>Najnoviji recepti</h1></div>";
+        if ($query_1->have_posts()) :
+			while ($query_1->have_posts()) : $query_1->the_post();
+            echo '<article id="post-' . get_the_ID() . '" class="index-class">';
+                $image = get_field('image');
+
+                if (!empty($image)) {
+                ?>
+                    <div class="post-thumbnail">
+                        <a href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+                            <img src="<?php echo $image; ?>" alt="slika recepta" width="400" height="300">
+                        </a>
+                    </div>
+                    <header class="entry-header">
+                        <?php the_title('<h2 class="entry-title" <a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
+                    </header>
+                <?php }
+                echo'</article>';
+				
+			endwhile;
+			wp_reset_postdata();
+		else :
+			echo 'Nema dostupnih objava';
+		endif;
+        echo"<div class='div-naslov'><h1 class='naslovi'>Najnoviji saveti</h1></div>";
         while (have_posts()) :
             the_post();
             ?>
